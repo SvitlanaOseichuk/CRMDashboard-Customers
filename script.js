fae5c31b569eed0fcdc3fde241cae13a8a3b9508
SvitlanaOseichuk <141953748+SvitlanaOseichuk@users.noreply.github.com>
2024-12-08 18:27:36 +0200

    add versel

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
   const sidebar = document.querySelector('.sidebar');
+  const toggleButton = document.querySelector('.sidebar__toggle');
+  const logo = document.querySelector('.sidebar__logo');
 
-  hamburgerMenu.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
+  toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+    if (sidebar.classList.contains('show')) {
+      toggleButton.style.display = 'none';
+    } else {
+      toggleButton.style.display = 'block';
+    }
   });
 });
