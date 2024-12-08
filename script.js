f833e24ad29e5b6e113b0621f7ca3dd2b1753029
SvitlanaOseichuk <141953748+SvitlanaOseichuk@users.noreply.github.com>
2024-12-08 21:23:52 +0200

    add style and js

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sidebar = document.querySelector('.sidebar');
   const toggleButton = document.querySelector('.sidebar__toggle');
-  const logo = document.querySelector('.sidebar__logo');
-
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  
   toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('show');
     if (sidebar.classList.contains('show')) {
       toggleButton.style.display = 'none';
     } else {
       toggleButton.style.display = 'block';
+    }
+  });
+
+  document.addEventListener('click', (event) => {
+    if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
+      sidebar.classList.remove('show');
+      hamburgerMenu.style.display = 'flex';
+    }
+  });
+
+  const filterLink = document.getElementById('filter-active');
+  const rows = document.querySelectorAll('.customers__table tbody tr');
+
+  filterLink.addEventListener('click', (event) => {
+    event.preventDefault(); 
+
+    if (filterLink.textContent.trim() === 'Active Members') {
+      rows.forEach(row => {
+        const statusCell = row.querySelector('.status');
+        if (statusCell && statusCell.classList.contains('status--active')) {
+          row.style.display = ''; 
+        } else {
+          row.style.display = 'none'; 
+        }
+      });
+      filterLink.textContent = 'Show All'; 
+    } else {
+      rows.forEach(row => {
+        row.style.display = ''; 
+      });
+      filterLink.textContent = 'Active Members'; 
     }
   });
 });
